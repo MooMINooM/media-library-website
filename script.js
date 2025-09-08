dfdbec48f44d045f78a49bc50348793f07a6f5c2
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-08 14:38:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ function formatDate(dateString) {
 
 // --- Data Fetching ---
 async function fetchData() {
-    if (!SCRIPT_URL || SCRIPT_URL === 'https://script.google.com/macros/s/AKfycbxgxfZ5SB9Um4HftajMJS6RJMG9kwd6hVjKz_DYTxDgQOB9qk1Xxl0mS1dr5YuoIFi-/exec') {
+    if (!SCRIPT_URL || SCRIPT_URL === 'วาง URL ของเว็บแอปจาก Apps Script ที่นี่') {
         showError('เกิดข้อผิดพลาด: กรุณาตั้งค่า SCRIPT_URL ในไฟล์ script.js ก่อน\nโปรดตรวจสอบว่าคุณได้ใส่ URL ของเว็บแอปถูกต้องแล้ว และได้ Deploy สคริปต์เวอร์ชันล่าสุดแล้ว');
         return;
     }
