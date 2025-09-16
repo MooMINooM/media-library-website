7b5ac7b509e6f5fd30f35d2950da4efb6c296d5a
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-17 01:22:19 +0700

    Create inno.js

---
+++ js/inno.js
@@ -0,0 +1,14 @@
+// --- Static data for Innovations ---
+export const STATIC_INNOVATIONS_DATA = [
+    {
+        title: 'Mathopoly เกมเศรษฐีคณิตศาสตร์',
+        creator: 'นายญาณวุฒิ บุตรสมบัติ',
+        category: 'สื่อดิจิทัล',
+        subject: 'คณิตศาสตร์',
+        grade: 'ทุกระดับชั้น',
+        uploadDate: '2024-08-15',
+        description: 'Mathopoly คือการบูรณาการเกมเศรษฐีเข้ากับการแก้โจทย์คณิตศาสตร์ โดยมีพื้นหลังเป็นนักคณิตศาสตร์คนสำคัญของโลกในแต่ละยุคสมัย',
+        fileUrl: 'https://moominoom.github.io/Mathopoly/',
+        coverImageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczPjZCDDrFfI69L94kZ3EPEut9p3a3gZn_z7uxlHrAM1JO2IHwXb1D1YGOzMrBFQXeBkUOxfrWtD2tR2aqF90QCPmXhF8e8Q4BkFnSf6LQOj3-X6dtADe_sst8zU-ppFtvPTANJ0a3kyUbkqKhO2sZDj=w1749-h954-s-no?authuser=0'
+    },
+];
