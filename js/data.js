63749e6fdeb5e86e8565417a9958ac0aaa2fe644
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-16 18:29:12 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,199 +1,190 @@
-/*:
- * @target MZ
- * @plugindesc ระบบครบ Stardew Lite: กลางวัน-กลางคืน, ฤดูกาล, ปลูกผัก, เศรษฐกิจ, HUD, ร้านค้า, รดน้ำ, สัตว์เลี้ยง v1.2
- * @author GPT
- */
-
-(() => {
-  const Seasons = ["Spring", "Summer", "Fall", "Winter"];
-  const DaysPerSeason = 30;
-
-  const Crops = {
-    1: { name: "Carrot", growDays: 4, seasons: ["Spring"], itemId: 10, basePrice: 50 },
-    2: { name: "Corn", growDays: 6, seasons: ["Summer", "Fall"], itemId: 11, basePrice: 80 },
-  };
-
-  const Economy = {
-    Spring: 1.0,
-    Summer: 1.2,
-    Fall: 1.5,
-    Winter: 0.8
-  };
-
-  const farmKey = "StardewFarmData";
-
-  const Time = {
-    hour: 6,
-    minute: 0,
-    day: 1,
-    seasonIndex: 0,
-
-    advanceTime(minutes) {
-      this.minute += minutes;
-      if (this.minute >= 60) {
-        this.minute -= 60;
-        this.hour++;
-      }
-      if (this.hour >= 24) {
-        this.hour = 6;
-        this.minute = 0;
-        this.day++;
-        if (this.day > DaysPerSeason) {
-          this.day = 1;
-          this.seasonIndex = (this.seasonIndex + 1) % Seasons.length;
-        }
-        updateCropsGrowth();
-      }
+// --- Static data for Personnel Structure ---
+export const STATIC_PERSONNEL_DATA = [
+    { 
+        name: 'นางสาวสุพิศน์ พันธุนันท์', 
+        role: 'ผู้อำนวยการสถานศึกษา',
+        academicStanding: 'ชำนาญการพิเศษ',
+        education: '- ค.บ.ภาษาอังกฤษ มหาวิทยาลัยราชภัฏอุดรธานี\n- กศ.ม.การบริหารการศึกษา มหาวิทยาลัยมหาสารคาม',
+        class: '-',
+        tel: '081-234-5678',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczM2HELcaPEIJuj0jR2qHvYmQmbveaCqqoND2vtSH6LGlRkDGL4JIZzmzP09FNq5ElNFNijen3AmNDKiT9Kw2J4zeD83RnNpO6D62PKEUk6ucWG7S29mwiYM1mXyx4op0auWBt6qwJltkqvVd2AkacZd=w693-h923-s-no-gm?authuser=0' 
     },
-
-    getSeason() {
-      return Seasons[this.seasonIndex];
+    { 
+        name: 'นายสมยศ บุญประคม', 
+        role: 'ครู',
+        academicStanding: 'ผู้ทรงคุณค่า',
+        education: '- ค.บ.พลศึกษา วิทยาลัยครูอุดรธานี\n- กศ.ม.เทคโนโลยีทางการศึกษา มหาวิทยาลัยมหาสารคาม',
+        class: '-',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczOfU073oWUhWyzdyJnZV3lYv3a3JlgZ_5lwebsbvRvJg9ve3FD7HgmxAYyq50xzFZ6mWS9MHFje9ShC493sAUw2qj-xPFlWjhcHPR3r9mmkDk669snXrECd1ktlkiTqGUIvBO_gM1L9RcL0Ph9uFhLT=w693-h923-s-no-gm?authuser=0'
     },
-
-    getTimeString() {
-      return `${this.hour}:${this.minute.toString().padStart(2, '0')}`;
+    { 
+        name: 'นางมัณฑนา บุญประคม', 
+        role: 'ครู',
+        academicStanding: 'เชี่ยวชาญ',
+        education: '- ค.บ.(เกียรตินิยมอันดับ ๒) ประถมศึกษา วิทยาลัยครูอุบลราชธานี\n- ศษ.ม.หลักสูตรและการสอน วิทยาลัยพิชญบัณฑิต',
+        class: 'ประถมศึกษาปีที่ 1',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczNyYJV9Sb9Jyn0-_x-bGrS_8VnwVBMHUmvPER5u9kgyk-7mMJ94fNo8aNaXfcfu0U4vHs0lyPn7WH3KKSVs4-TAp-L4FEJEap7h_zie1xG22tHCS_byYJMleWLigxrWz3vxUz8JlaYW4pYmTF2rmsjP=w693-h923-s-no-gm?authuser=0'
     },
+     { 
+        name: 'นางบัณฑิตา วรจิตร', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการพิเศษ',
+        education: '- ค.บ.การศึกษาปฐมวัย มหาวิทยาลัยราชภัฏอุดรธานี\n- ศษ.ม.การบริหารการศึกษา มหาวิทยาลัยกรุงเทพธนบุรี',
+        class: 'อนุบาล 3',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczOvZ8qI-FnKs1JheUxJTIVNbO42g4VYJJaU4kkczY0Swg0RzvZCP_zL54pG1c2IWIJFmngjGaOKj3_HzzX8JAe9K-kubp5QdO5P2-a2WY1gxb1HOWWpPbot15bq8VY8vfHzsUJzF7lQhfm-x5r1HJFz=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางปริฉัตร ไชยอุบล', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการพิเศษ',
+        education: '- วท.บ.วิทยาศาสตร์สิ่งแวดล้อม มหาวิทยาลัยราชภัฏอุดรธานี\n- ป.บัณฑิตวิชาชีพครู มหาวิทยาลัยราชภัฏอุดรธานี \n- ศษ.ม.การบริหารการศึกษา มหาวิทยาลัยรามคำแหง',
+        class: 'ประถมศึกษาปีที่ 6',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczOiOOQOJolSM3KBrQfY3-n7cvLQ746I3_YQtsTy5G00moXHhh8Cji4WV4GvCEMcJUuevM9r-Jje4ET62SUkN6widCgmojbgx_oJwpLcLaWYNQBnNfXiLTF-1pb6HSVIgvmdJQPo2f_E5EMojv7ruDtE=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นายวิรุฬห์ บงแก้ว', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการพิเศษ',
+        education: '- ค.บ.วิทยาศาสตร์ มหาวิทยาลัยราชภัฏเลย',
+        class: 'มัธยมศึกษาปีที่ 3',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczPNb6WJYiWbSEVCkhL3cGUFvkXCEgVEwt46pJKAt1gtaGOX6OsGWWVNP1cl-moxubzYdel9uyFhMU0isvWDfH16NA9JwsyGjC9fiONb3i03lg3INGV-b5zj0A6kq68XkQwdfts5bwK6oG1BCqSwlDza=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางพิมพ์ภัช กัณหาสุระ', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการพิเศษ',
+        education: '- ค.บ.คอมพิวเตอร์ศึกษา มหาวิทยาลัยราชภัฏอุดรธานี\n- ค.ม.หลักสูตรและการสอน มหาวิทยาลัยราชภัฏอุดรธานี',
+        class: 'มัธยมศึกษาปีที่ 1',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczPoPKS-0rz95sqlyF9LT0vLoBhnfycS1EiPnbw9grWvR1vFjST5FviPQHZvpGJd-96kNRHEapLdzrolk-_Qe-etcg21DH7rWA52jphI1wIsewOWhGV5ZxQwfJ30Hn8gpqRI65KbN3H_Jz3lkqd5mJBf=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางปิยะพร โพธิ์วงษ์', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการพิเศษ',
+        education: '- ค.บ.การศึกษาปฐมวัย มหาวิทยาลัยราชภัฏอุดรธานี',
+        class: 'อนุบาล 2',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczMS1B234kUdNZdcofvLK1WQGDt1C8sOvPcCk6Izg1fCntJWKsopzFOCXASXb9beR5K1pBGWiyMZHIMAFFuoh22l377JDR4scqOCaq4meEFI8nSr_FyOuVWR31S2Uv87sm6JrPgtEqZN5ikc6c1jAuGV=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางสุดดารัตน์ ไทยมา', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการ',
+        education: '- ศศ.บ.ประวัติศาสตร์ มหาวิทยาลัยนเรศวร\n- บธ.บ.การท่องเที่ยว มหาวิทยาลัยนเรศวร\n- ศษ.ม.หลักสูตรและการสอน มหาวิทยาลัยกรุงเทพธนบุรี',
+        class: 'ประถมศึกษาปีที่ 5',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczPSXHwKmL5eA5hwh_HSJcf0C-fBN9AxfRgs4KvP0ESZpMKozyi0ZKdUbAGrJM8tq28laI7a5ix19-loN-f8yfTl25ispNxksoGWH--0Mgce-oq_-Y8QVpWQPDsquDuHP5MET-mxreZgiuKnJ1HmKvwS=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นายธีรโชติ พันธ์เดช', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการ',
+        education: '- ศศ.บ.(เกียรตินิยมอันดับ ๒) ภาษาอังกฤษ มหาวิทยาลัยราชภัฏอุดรธานี\n- ศษ.บ.การแนะแนว มหาวิทยาลัยสุโขทัยธรรมาธิราช\n- ศษ.ม.บริหารการศึกษา วิทยาลัยพิชญบัณฑิต',
+        class: 'มัธยมศึกษาปีที่ 2',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczP3S_eGS1o4F-ash-yLX6bPFzpWF_WhD7uwaK0Z8zmN7Lx8eIJFp2U8UVVB3H9qNwkK4SGxnSEyaMqCAIrOgSvh786CaKie0s2ZkkvaCymmMgmNO2Sv4z0MZSX40gUxeSI6IOFyb01zMQzfP3fKHYam=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางสาววิมลดา นาสมยนต์', 
+        role: 'ครู',
+        academicStanding: 'ชำนาญการ',
+        education: '- ค.บ.(เกียรตินิยมอันดับ ๒) ดนตรีศึกษา มหาวิทยาลัยราชภัฏมหาสารคาม',
+        class: 'ประถมศึกษาปีที่ 4',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczMmjRUj2wUPjQqckqr1TTB4sAwDZxHjKjgtj8dwuvMUfmsnF2MH4l_ifjOkr4EcpIwZAwSC1Eu8i0aNWBPpmMDVlrcTddy3idAC79sMXQyj2hVZRngB5ZUh3CUa79IrFPEjZPl6NnEadJ7kNMGdHGy1=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นายดอกคูณ พูดเพราะ', 
+        role: 'ครู',
+        academicStanding: '-',
+        education: '- วท.บ.คณิตศาสตร์ มหาวิทยาลัยราชภัฏอุดรธานี\n- ป.บัณฑิตวิชาชีพครู มหาวิทยาลัยราชภัฏอุดรธานี',
+        class: 'ประถมศึกษาปีที่ 6',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczMsOzLIk7mORWFfmsVQHQX8cXANSQ4pcKT7FMqOViL2eup1ay8zEYApWl5hxIIipt_ISFroy6PQfuw6bNjnQrW13Z84e7_rVkeeoy_1_vO2pXY5o_dzt28aERhQXENPRIWcB-HXQxnzsaiwKS8XxHuX=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางสาวมณีรัตน์ อาจสมดี', 
+        role: 'ครู',
+        academicStanding: '-',
+        education: '- ศษ.บ.ภาษาอังกฤษ มหาวิทยาลัยรามคำแหง',
+        class: 'ประถมศึกษาปีที่ 2',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczOEdF1ISV1dx24tAgpwDEGK8-igunNHDlrygUA4r7-cIYncFJOWloOHxLT6DWTpBR6uZjoJiJfJrvjG6sasW4PJy2ekN3_8_2H_eWkVpU1S5ovaZEe8JjyWy-BgxJgp4UUqKjMZVqJtJRZtUoNdBWbC=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นายญาณวุฒิ บุตรสมบัติ', 
+        role: 'ครู',
+        academicStanding: '-',
+        education: '- กศ.บ.คณิตศาสตร์ มหาวิทยาลัยมหาสารคาม',
+        class: 'ประถมศึกษาปีที่ 3',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczOb5YshtBJ8QpSBiohDDXNTBZxaZWNZl_V-FOQ9G_690GRa5cFdUyxRwAbUC5yh422QaTSLU5eRp_xdU5e3hO5qrViPuEWB5PA7q1A5Xd13yU5czu_Ea7BcU3YerXjUXv65PTneb7cfZrjqvA-sNrbP=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นายวุฒิพงษ์ เจนสระคู', 
+        role: 'ครูผู้ช่วย',
+        academicStanding: '-',
+        education: '- ค.บ.ภาษาไทย มหาวิทยาลัยราชภัฏเลย',
+        class: 'ประถมศึกษาปีที่ 2',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczMVYN45CYqXeGQAiM9yp31NqOzduG_XdHAh1fZL9GM_z7WUP4-p-EE46wlb0mX8E7cRBZtyMjTuQTykSgexsBgWoGgLTzcTrCW54ea-DnkZshCE5JbVpCQH2R1FOGZCdFZYjZ-Z5SpMX4tvjOPLbgjy=w331-h402-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางสาววิภาพร อุดทา', 
+        role: 'ครูผู้ช่วย',
+        academicStanding: '-',
+        education: '- ค.บ.ภาษาไทย มหาวิทยาลัยนครพนม',
+        class: 'ประถมศึกษาปีที่ 1',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczPG3vOyzHs4ws5YWyGe-L03mC3mrTCG9NPLpefm5DRR97YVeV2yw_NDxUOpv6chVfrgqqKqLz_JRwXrtm8L5_OpPgVHdNAS67LLVQyhLzav2-8HN_OMwuOTjd8bP2FMwIlVOH2p_4Q7gqLI0qHd0Txn=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นางสาวอรุณ สายอ่อนตา', 
+        role: 'ครูธุรการ',
+        academicStanding: '-',
+        education: '- ค.บ.วิทยาศาสตร์ทั่วไป มหาวิทยาลัยราชภัฏเลย',
+        class: '-',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczNv3nxrVxEm29uVlNVcCakHOvOdafJ93eWwrkfre-W5TYVL3aDgx_1N1SSPpUO0mg7JsSB7ZcvoJMX2pMx1hcR5d4Yas1RIItsibRrsqyQ1ZY0eS7baQ6Vkyz_4BI9gMgUUaeKtD_-xyI8gilGUoeC_=w693-h923-s-no-gm?authuser=0'
+    },
+    { 
+        name: 'นายสุชาติ อาสาสะนา', 
+        role: 'ช่างไม้ 4',
+        academicStanding: '-',
+        education: '- ปวส.ไฟฟ้ากำลัง',
+        class: '-',
+        tel: '082-345-6789',
+        imageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczOxWP1amBpdQMTQeuJIN7691QPuD-OQPV5MJUimVHddz4QwYRVd1Uk7Jz7u7hU9SZ_HHuBzlWP-IDskqAN9NcIIZYNM3ZCno11yNPvScx9UeLvUIpuB2A9x6zcniVGcB8kmIaVjD4BDXTXcSd8DyeYT=w693-h923-s-no-gm?authuser=0'
+    },
+];
 
-    getDateString() {
-      return `${this.getSeason()} Day ${this.day}`;
-    }
-  };
+// --- Static data for Student Council Structure ---
+export const STATIC_STUDENT_COUNCIL_DATA = [
+    { name: 'ประธานนักเรียน', class: 'ประถมศึกษาปีที่ 6', role: 'ประธานนักเรียน', imageUrl: 'YOUR_IMAGE_URL_HERE' },
+];
 
-  const getFarmData = () => {
-    if (!$gameMap._farmData) $gameMap._farmData = {};
-    return $gameMap._farmData;
-  };
+// --- Static data for School Board Structure ---
+export const STATIC_SCHOOL_BOARD_DATA = [
+    { name: 'ประธานกรรมการ', role: 'ประธานกรรมการสถานศึกษา', imageUrl: 'YOUR_IMAGE_URL_HERE' },
+];
 
-  Game_Interpreter.prototype.plantCrop = function(seedId, x, y) {
-    const crop = Crops[seedId];
-    if (!crop || !crop.seasons.includes(Time.getSeason())) return;
-    const farm = getFarmData();
-    const key = `${x},${y}`;
-    farm[key] = {
-      seedId: seedId,
-      dayPlanted: Time.day,
-      seasonPlanted: Time.getSeason(),
-      daysWithoutWater: 0
-    };
-    $gameMap.requestRefresh();
-  };
-
-  Game_Interpreter.prototype.waterCrop = function(x, y) {
-    const farm = getFarmData();
-    const key = `${x},${y}`;
-    const data = farm[key];
-    if (!data) return;
-    data.daysWithoutWater = 0;
-    console.log(`รดน้ำที่ ${key}`);
-  };
-
-  function updateCropsGrowth() {
-    const farm = getFarmData();
-    for (const key in farm) {
-      const data = farm[key];
-      const crop = Crops[data.seedId];
-
-      if (data.seasonPlanted !== Time.getSeason()) {
-        delete farm[key];
-        continue;
-      }
-
-      data.daysWithoutWater++;
-      if (data.daysWithoutWater > 2) {
-        delete farm[key];
-        console.log(`พืชที่ ${key} ตายเพราะขาดน้ำ`);
-      }
-    }
-    $gameMap.requestRefresh();
-  }
-
-  Game_Interpreter.prototype.harvestCrop = function(x, y) {
-    const farm = getFarmData();
-    const key = `${x},${y}`;
-    const data = farm[key];
-    if (!data) return;
-    const crop = Crops[data.seedId];
-    const daysGrown = Time.day - data.dayPlanted;
-    if (daysGrown >= crop.growDays) {
-      const multiplier = Economy[Time.getSeason()];
-      const price = Math.floor(crop.basePrice * multiplier);
-      $gameParty.gainItem($dataItems[crop.itemId], 1);
-      $gameParty.gainGold(price);
-      delete farm[key];
-      $gameMap.requestRefresh();
-    }
-  };
-
-  Scene_Map.prototype.update = function() {
-    Scene_Base.prototype.update.call(this);
-    if (Graphics.frameCount % 300 === 0) {
-      Time.advanceTime(10);
-      $gameMap.requestRefresh();
-    }
-    this.updateHud();
-  };
-
-  Scene_Map.prototype.createAllWindows = function() {
-    Scene_Message.prototype.createAllWindows.call(this);
-    this.createHudWindow();
-    this.createCalendarWindow();
-  };
-
-  Scene_Map.prototype.createHudWindow = function() {
-    const rect = new Rectangle(0, 0, 300, 100);
-    this._hudWindow = new Window_HUD(rect);
-    this.addChild(this._hudWindow);
-  };
-
-  Scene_Map.prototype.createCalendarWindow = function() {
-    const rect = new Rectangle(Graphics.width - 300, 0, 300, 100);
-    this._calendarWindow = new Window_Calendar(rect);
-    this.addChild(this._calendarWindow);
-  };
-
-  Scene_Map.prototype.updateHud = function() {
-    if (this._hudWindow) this._hudWindow.refresh();
-    if (this._calendarWindow) this._calendarWindow.refresh();
-  };
-
-  class Window_HUD extends Window_Base {
-    initialize(rect) {
-      super.initialize(rect);
-      this.refresh();
-    }
-    refresh() {
-      this.contents.clear();
-      this.drawText(Time.getDateString(), 0, 0, 280);
-      this.drawText(Time.getTimeString(), 0, this.lineHeight(), 280);
-    }
-  }
-
-  class Window_Calendar extends Window_Base {
-    initialize(rect) {
-      super.initialize(rect);
-      this.refresh();
-    }
-    refresh() {
-      this.contents.clear();
-      this.drawText(`Calendar`, 0, 0, 280);
-      this.drawText(Time.getSeason(), 0, this.lineHeight(), 280);
-      this.drawText(`Day ${Time.day} / ${DaysPerSeason}`, 0, this.lineHeight() * 2, 280);
-    }
-  }
-
-  Game_Interpreter.prototype.openFarmShop = function() {
-    const goods = [
-      [0, 10, 0, 0],
-      [0, 11, 0, 0]
-    ];
-    SceneManager.push(Scene_Shop);
-    SceneManager.prepareNextScene(goods, false);
-  };
-
-  Game_Interpreter.prototype.collectAnimalProduct = function(itemId, goldAmount) {
-    $gameParty.gainItem($dataItems[itemId], 1);
-    $gameParty.gainGold(goldAmount);
-    console.log(`เก็บผลผลิตสัตว์ ได้ ${goldAmount}G`);
-  };
-
-})();
+// --- Static data for Student Information ---
+export const STATIC_STUDENT_DATA = [
+     { grade: 'อนุบาล 2', boys: 2, girls: 7, total: 9 },
+    { grade: 'อนุบาล 3', boys: 11, girls: 7, total: 18 },
+    { grade: 'ประถมศึกษาปีที่ 1', boys: 14, girls: 10, total: 24 },
+    { grade: 'ประถมศึกษาปีที่ 2', boys: 8, girls: 11, total: 19 },
+    { grade: 'ประถมศึกษาปีที่ 3', boys: 14, girls: 8, total: 22 },
+    { grade: 'ประถมศึกษาปีที่ 4', boys: 12, girls: 8, total: 20 },
+    { grade: 'ประถมศึกษาปีที่ 5', boys: 6, girls: 14, total: 20 },
+    { grade: 'ประถมศึกษาปีที่ 6', boys: 12, girls: 9, total: 21 },
+    { grade: 'มัธยมศึกษาปีที่ 1', boys: 7, girls: 8, total: 15 },
+    { grade: 'มัธยมศึกษาปีที่ 2', boys: 4, girls: 12, total: 16 },
+    { grade: 'มัธยมศึกษาปีที่ 3', boys: 0, girls: 3, total: 3 },
+];
