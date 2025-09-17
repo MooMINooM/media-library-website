980d3e75e2da7d2f0d31daf0f5994065fe4630e7
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-17 17:14:23 +0700

    Update news.js

--- js/news.js
+++ js/news.js
@@ -2,27 +2,27 @@
 export const STATIC_NEWS_DATA = [
     {
         title: 'พิธีไหว้ครูประจำปีการศึกษา 2568',
-        url: 'https://lh3.googleusercontent.com/pw/AP1GczPLc7YnQ3J4PvHz4rG77gF3dmfkkpdLGZUkgtat3ncD9wC9wb3OegXqwGhPpuKDbZtTGCzjzF9-ZJsGA2BfNhyg-1VOcxZZyyahBdrGhfOKafUyzx0aZv57_NrtSKMsSGTiwGvUS74qCRntC6vtSxY=w653-h923-s-no-gm?authuser=1', // Placeholder link
+        link: 'https://www.facebook.com/', // Placeholder link
         date: '2025-06-12'
     },
     {
         title: 'โรงเรียนบ้านภูพระโนนผักหวานจัดกิจกรรมวันแม่แห่งชาติ ประจำปี 2568',
-        url: '#',
+        link: '#',
         date: '2025-08-12'
     },
     {
         title: 'ขอแสดงความยินดีกับนักเรียนที่ได้รับรางวัลการแข่งขันคัดลายมือระดับเขตพื้นที่ฯ',
-        url: '#',
+        link: '#',
         date: '2025-07-29'
     },
     {
         title: 'คณะครูและนักเรียนร่วมกันจัดกิจกรรมแห่เทียนเข้าพรรษา ณ วัดป่าภูพระ',
-        url: '#',
+        link: '#',
         date: '2025-07-15'
     },
     {
         title: 'กิจกรรม Big Cleaning Day เตรียมความพร้อมก่อนเปิดภาคเรียน',
-        url: '#',
+        link: '#',
         date: '2025-05-10'
     }
 ];
