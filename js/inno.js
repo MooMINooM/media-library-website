2a3b9bda373a67ce37290cc452a77eef7e092c5a
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-17 10:42:16 +0700

    Update inno.js

--- js/inno.js
+++ js/inno.js
@@ -6,9 +6,20 @@ export const STATIC_INNOVATIONS_DATA = [
         category: 'สื่อดิจิทัล',
         subject: 'คณิตศาสตร์',
         grade: 'ทุกระดับชั้น',
-        uploadDate: '2024-08-15',
+        uploadDate: '2025-08-15',
         description: 'Mathopoly คือการบูรณาการเกมเศรษฐีเข้ากับการแก้โจทย์คณิตศาสตร์ โดยมีพื้นหลังเป็นนักคณิตศาสตร์คนสำคัญของโลกในแต่ละยุคสมัย',
         fileUrl: 'https://moominoom.github.io/Mathopoly/',
         coverImageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczPjZCDDrFfI69L94kZ3EPEut9p3a3gZn_z7uxlHrAM1JO2IHwXb1D1YGOzMrBFQXeBkUOxfrWtD2tR2aqF90QCPmXhF8e8Q4BkFnSf6LQOj3-X6dtADe_sst8zU-ppFtvPTANJ0a3kyUbkqKhO2sZDj=w1749-h954-s-no?authuser=0'
+    },
+    {
+        title: 'บทเรียนสำเร็จรูปเรื่องอาหาร',
+        creator: 'นางปริฉัตร ไชยอุบล',
+        category: 'บทเรียนสำเร็จรูป',
+        subject: 'วิทยาศาสตร์',
+        grade: 'ประถมศึกษาปีที่ 3',
+        uploadDate: '2025-09-17',
+        description: 'บทเรียนสำเร็จรูปประกอบการเรียนรู้เรื่องอาหาร ระดับชั้นประถมศึกษาปีที่ 3',
+        fileUrl: 'https://drive.google.com/file/d/1n79qWGOow0MTGvHifjNVn44TU5oil7Ox/view?usp=drive_link',
+        coverImageUrl: 'https://lh3.googleusercontent.com/pw/AP1GczOULH8ttXI1OCIwC7UW24l-g-QEp1892pyz8MtcdsRzicTZd-IpRbrF7y_bPokGUqwI8SP89ob2ftWKaNzzEqRNPccaxWyqGAdENaWY0leWZ_gtoSjN-riV8wJKcH-pBUmqfebGFgzEb0Dd-6C4birE=w1305-h923-s-no-gm?authuser=1'
     },
 ];
