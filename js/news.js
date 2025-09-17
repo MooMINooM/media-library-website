486e8d4a8de2eaaab4dc3873aa2eaad208e2c66c
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-17 17:28:51 +0700

    Update news.js

--- js/news.js
+++ js/news.js
@@ -1,19 +1,19 @@
 // --- Static data for News ---
 export const STATIC_NEWS_DATA = [
     {
-        title: 'พิธีไหว้ครูประจำปีการศึกษา 2568',
+        title: 'พิธีไหว้ครู ประจำปีการศึกษา 2568',
         link: 'https://lh3.googleusercontent.com/pw/AP1GczPLc7YnQ3J4PvHz4rG77gF3dmfkkpdLGZUkgtat3ncD9wC9wb3OegXqwGhPpuKDbZtTGCzjzF9-ZJsGA2BfNhyg-1VOcxZZyyahBdrGhfOKafUyzx0aZv57_NrtSKMsSGTiwGvUS74qCRntC6vtSxY=w653-h923-s-no-gm?authuser=1', // Placeholder link
         date: '2025-06-12'
     },
     {
-        title: 'โรงเรียนบ้านภูพระโนนผักหวานจัดกิจกรรมวันแม่แห่งชาติ ประจำปี 2568',
-        link: '#',
-        date: '2025-08-12'
+        title: 'วันต่อต้านยาเสพติด ประจำปี 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczM6QraYMA2w5kf9GSu0g5nRwrh3c955wcp6EdAFHOYk53nshVqfCIGhd90nFgX2rQImWDrS4eXvNtI2zQArPHnwUw2eAnOSHbIphf94m7D-SuT5yPhTkbldY3-hiQaFVsBhjhIZXpa11CyWHijsNho=w653-h923-s-no-gm?authuser=1',
+        date: '2025-06-26'
     },
     {
-        title: 'ขอแสดงความยินดีกับนักเรียนที่ได้รับรางวัลการแข่งขันคัดลายมือระดับเขตพื้นที่ฯ',
-        link: '#',
-        date: '2025-07-29'
+        title: 'กิจกรรมวันสุนทรภู่ และวันภาษาไทยแห่งชาต ประจำปีการศึกษา 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczMrpNB9Vc3eb0s2VBLezMjp1Mv5G43oc_KVcnBVIx76iElVLhpXdpXWXs6SWCBefPVuR21WbDjvVo99BnpcA-N4BUJ-oJdZ26iLc01vcq02BXEMA7zK3fbN4BdWSNoCd2TNJc9HvXyOCyzP5zU6u_E=w653-h923-s-no-gm?authuser=1',
+        date: '2025-06-27'
     },
     {
         title: 'คณะครูและนักเรียนร่วมกันจัดกิจกรรมแห่เทียนเข้าพรรษา ณ วัดป่าภูพระ',
