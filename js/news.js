2428964cbfdb7b054533c8748479c6145880d9da
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-17 09:33:24 +0700

    Create news.js

---
+++ js/news.js
@@ -0,0 +1,31 @@
+// --- Static data for News ---
+export const STATIC_NEWS_DATA = [
+    {
+        title: 'พิธีไหว้ครูประจำปีการศึกษา 2568',
+        fileUrl: 'https://lh3.googleusercontent.com/pw/AP1GczPLc7YnQ3J4PvHz4rG77gF3dmfkkpdLGZUkgtat3ncD9wC9wb3OegXqwGhPpuKDbZtTGCzjzF9-ZJsGA2BfNhyg-1VOcxZZyyahBdrGhfOKafUyzx0aZv57_NrtSKMsSGTiwGvUS74qCRntC6vtSxY=w653-h923-s-no-gm?authuser=1', // Replace with the actual link to the PDF or document
+        category: 'กิจกรรม',
+        publishDate: '2025-06-12',
+        summary: '-'
+    },
+    {
+        title: 'กิจกรรมวันวิทยาศาสตร์แห่งชาติ ประจำปี 2568',
+        fileUrl: '#', // Replace with the actual link
+        category: 'ข่าวกิจกรรม',
+        publishDate: '2025-08-18',
+        summary: 'ภาพบรรยากาศกิจกรรมวันวิทยาศาสตร์แห่งชาติที่จัดขึ้นในวันที่ 18 สิงหาคม 2568 มีการจัดแสดงโครงงานของนักเรียนและการแข่งขันตอบปัญหาวิทยาศาสตร์...'
+    },
+    {
+        title: 'ขอเชิญร่วมงานประชุมผู้ปกครอง ภาคเรียนที่ 2/2568',
+        fileUrl: '#', // Replace with the actual link
+        category: 'จดหมายเชิญ',
+        publishDate: '2025-10-25',
+        summary: 'โรงเรียนขอเรียนเชิญผู้ปกครองนักเรียนทุกระดับชั้นเข้าร่วมประชุมเพื่อรับฟังนโยบายและแนวทางการจัดการเรียนการสอนในภาคเรียนที่ 2 ในวันที่ 1 พฤศจิกายน 2568'
+    },
+    {
+        title: 'โรงเรียนได้รับรางวัลสถานศึกษาปลอดภัยดีเด่น',
+        fileUrl: '#', // Replace with the actual link
+        category: 'ข่าวประชาสัมพันธ์',
+        publishDate: '2025-07-30',
+        summary: 'ขอแสดงความยินดีกับโรงเรียนบ้านภูพระโนนผักหวานที่ได้รับรางวัลสถานศึกษาปลอดภัยดีเด่น ประจำปี 2568 จากกรมสวัสดิการและคุ้มครองแรงงาน'
+    },
+];
