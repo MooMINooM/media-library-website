12327220c4d2f7202da9f0cbe83de7dfbb44a231
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-17 20:19:55 +0700

    Update news.js

--- js/news.js
+++ js/news.js
@@ -11,18 +11,38 @@ export const STATIC_NEWS_DATA = [
         date: '2025-06-26'
     },
     {
-        title: 'กิจกรรมวันสุนทรภู่ และวันภาษาไทยแห่งชาต ประจำปีการศึกษา 2568',
+        title: 'วันสุนทรภู่ และวันภาษาไทยแห่งชาต ประจำปีการศึกษา 2568',
         link: 'https://lh3.googleusercontent.com/pw/AP1GczMrpNB9Vc3eb0s2VBLezMjp1Mv5G43oc_KVcnBVIx76iElVLhpXdpXWXs6SWCBefPVuR21WbDjvVo99BnpcA-N4BUJ-oJdZ26iLc01vcq02BXEMA7zK3fbN4BdWSNoCd2TNJc9HvXyOCyzP5zU6u_E=w653-h923-s-no-gm?authuser=1',
         date: '2025-06-27'
     },
     {
-        title: 'คณะครูและนักเรียนร่วมกันจัดกิจกรรมแห่เทียนเข้าพรรษา ณ วัดป่าภูพระ',
-        link: '#',
-        date: '2025-07-15'
+        title: 'วันคล้ายวันสถาปนาลูกเสือแห่งชาติ ประจำปี 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczPlDDB0wVoNCu3a_JeDcs0B5ik0XAxb4tXL_ZPV9j9zjTJqmKSwKiTm_25qU0MyTfk-c61SraQhXDWKIOIC32fIpg00ZlHqLd-84hHfj-Z2ZoOCvrHgxpkAxrnMjDxuj5O0D1MHJ3Ql8LYYMP3hwyA=w674-h954-s-no?authuser=0',
+        date: '2025-07-01'
     },
     {
-        title: 'กิจกรรม Big Cleaning Day เตรียมความพร้อมก่อนเปิดภาคเรียน',
-        link: '#',
-        date: '2025-05-10'
-    }
+        title: 'จิตอาสาเราทำความดีด้วยหัวใจ ประจำปี 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczPhAkK7zRc7sqi9Ouk0VOLQkYUKVFfc8FbwWnswslVu59gsNQYeGK7O5zHfy5zYGUmW8Aww2KDU9_fcBmMZilIoASJMuanMvDXt3nv7nK_aM4QgOR0kDDz3-ivjJJ0an2kR_066MlK8bDqI-tNgmnE=w674-h954-s-no?authuser=0',
+        date: '2025-07-03'
+    },
+    {
+        title: 'ทำบุญเนื่องในวันอาสาฬหบูชา และวันเข้าพรรษา ประจำปี 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczMM4S9q58hpo3HwjXOeXEWOTVTACCFaECK75giSYyB9wt-QeaBx24EXmwoomUmSWjF40Q4FgQrz_ZTgGeX9Hbkflk5bH4G4QCdtPsbkmEOSqBnZRPkNvxhXHMly4QtFW3jMRppRtM_TzPnnL9lVu14=w674-h954-s-no?authuser=0',
+        date: '2025-07-09'
+    },
+    {
+        title: 'โครงการฝึกอบรมอาสาสมัครป้องกันไฟป่า และยุวชนพิทักษ์ไฟป่า ประจำปี 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczMmQr_wPwJeablfStujnpninSJGM0TuCESPCGVLgoq3psgcqHHKkFDrnAJb57zjoCfkz4_YrS4I3s7mKLg9CFwtvR-RNE6efh30XLWTl5uV2xSP1Hn6rVUExGB9fAXCoPL7JkVeAxa1uPlQ0E3ULWM=w674-h954-s-no?authuser=0',
+        date: '2025-07-18'
+    },
+    {
+        title: 'กิจกรรมแข่งทักษะภาษาไทย ประจำปีการศึกษา 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczNIZpTrNeyFGn9xOi1wvyR8S_w2YlImADLHbZBp8So9ID-LitR1teiVKn3RRbB6Pe1zySzuRQSWF1BlR_eVpGWEvg4_TFVvoM6tLJk8h9fCBn6p-63TmJPpZ1c-kdFrv4o_ugUNR-rUUwVU0CekhoQ=w674-h954-s-no?authuser=0',
+        date: '2025-07-18'
+    },
+    {
+        title: 'อบรมการส่งเสริมสิทธิเสรีภาพและสิทธิมนุษยชน เพื่อแก้ปัญหาการละเมิดเสรีภาพในพื้นที่ ประจำปี 2568',
+        link: 'https://lh3.googleusercontent.com/pw/AP1GczMPtkk5X1P22OaIjZ2FZHMKPQplNL1AaLi47PTijcotRWc4KktpMv2g2b0ZBkiNSvvFu7Go0KvjKQdbXB4uoIKM47YoMOW7HHYpMZAfr_fXZpRTyxohG1oPLyTjXu1mOr-de5wsZ1nf6o5GSkndbVs=w674-h954-s-no?authuser=0',
+        date: '2025-07-22'
+    },
 ];
