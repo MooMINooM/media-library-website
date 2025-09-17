4dba6d1d139c31b9554c584124c1ef654c862912
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-17 19:31:59 +0700

    Create member.js

---
+++ js/member.js
@@ -0,0 +1,8 @@
+// js/member.js
+
+export const STATIC_PERSONNEL_HISTORY_DATA = [
+    { no: 1, name: 'นายมานะ ตั้งใจ', position: 'ครู', start: '16 พ.ค. 2548', end: '30 ก.ย. 2564' },
+    { no: 2, name: 'นางปิยะพร ใฝ่รู้', position: 'ครู', start: '16 พ.ค. 2552', end: '30 ก.ย. 2565' },
+    { no: 3, name: 'นายพัฒนา ชาติไทย', position: 'นักการภารโรง', start: '2 มิ.ย. 2540', end: '30 ก.ย. 2560' },
+    // เพิ่มข้อมูลตามจริง...
+];
