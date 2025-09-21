23eefe9dcdfe3c821ec2152a2d33a26163a28d29
MooMINooM <sirapopbutsombat@gmail.com>
2025-09-21 11:46:20 +0700

    Create teaward.js

---
+++ js/teaward.js
@@ -0,0 +1,73 @@
+// js/teaward.js
+// ไฟล์สำหรับเก็บข้อมูลผลงานของครู
+
+export const STATIC_TEACHER_AWARDS_DATA = [
+    {
+        name: 'นางสาวสุพิศ พันธุนันท์',
+        fileUrl: 'https://drive.google.com/drive/folders/1jpd1QADxyWG1kYsvwJBFJCq_m3bmNrrH?usp=drive_link' 
+    },
+    {
+        name: 'นายสมยศ บุญประคม',
+        fileUrl: 'https://drive.google.com/drive/folders/1uHSlKKoOf7sdiiuEij8IJCVErOzk7qaW?usp=drive_link' 
+    },
+    {
+        name: 'นางมัณฑนา บุญประคม',
+        fileUrl: 'https://drive.google.com/drive/folders/1YqYDlyGLYAckiVkLpOJE6Ae8nuDKn34W?usp=drive_link' 
+    },
+    {
+        name: 'นางบัณฑิตา วรจิตร',
+        fileUrl: 'https://drive.google.com/drive/folders/1D9eFAGB7iAIjvK6EDBc0p8oFKfj6uHlw?usp=drive_link' 
+    },
+    {
+        name: 'นายวิรุฬห์ บงแก้ว',
+        fileUrl: 'https://drive.google.com/drive/folders/1CaVRXWy10L44fAOrg34SE63qZnc0yF42?usp=drive_link' 
+    },
+    {
+        name: 'นางปริฉัตร ไชยอุบล',
+        fileUrl: 'https://drive.google.com/drive/folders/1ZocNz13Z39QU7moVVGgicCbfJFzpF2uX?usp=drive_link' 
+    },
+    {
+        name: 'นางพิมพ์ภัทร กัณหาสุระ',
+        fileUrl: 'https://drive.google.com/drive/folders/1yImPe9Xq5QoVck6UfJzeqcxzR_21Xb3N?usp=drive_link' 
+    },
+    {
+        name: 'นางปิยะพร โพธิ์วง',
+        fileUrl: 'https://drive.google.com/drive/folders/17atSNQ4t5WPHIuf0LXeiTfoOCYCxU2Rz?usp=drive_link' 
+    },
+    {
+        name: 'นายธีรโชติ พันธ์เดช',
+        fileUrl: 'https://drive.google.com/drive/folders/1PvX-JX_OdV4IrKcJej6akk0K-jQNwX08?usp=drive_link' 
+    },
+    {
+        name: 'นางสุดดารัตน์ ไทยมา',
+        fileUrl: 'https://drive.google.com/drive/folders/15wKXK_OyWXZwFecP3eRurq3GOH4m6324?usp=drive_link' 
+    },
+    {
+        name: 'นางสาววิมลดา นาสมยนต์',
+        fileUrl: 'https://drive.google.com/drive/folders/15wKXK_OyWXZwFecP3eRurq3GOH4m6324?usp=drive_link' 
+    },
+    {
+        name: 'นายดอกคูณ พูดเพราะ',
+        fileUrl: 'https://drive.google.com/drive/folders/1tmnuZ921mF8Gb1yI00G_CQZKJQmVGQJh?usp=drive_link' 
+    },
+    {
+        name: 'นางสาวมณีรัตน์ อาจสมดี',
+        fileUrl: 'https://drive.google.com/drive/folders/1yYocEc4mkXokbXRr2EiD-XwgJjMdaFHh?usp=drive_link' 
+    },
+    {
+        name: 'นายญาณวุฒิ บุตรสมบัติ',
+        fileUrl: 'https://drive.google.com/drive/folders/1AY31d4H9Qz_eKJ3Zsm7QgOAs4_Ko6p8J?usp=drive_link' 
+    },
+    {
+        name: 'นายวุฒิพงษ์ เจนสระคู',
+        fileUrl: 'https://drive.google.com/drive/folders/13RVD0ael-tO3JjFwoqea25Qrv_CwqFkx?usp=drive_link' 
+    },
+    {
+        name: 'นางสาววิภาพร อุดทา',
+        fileUrl: 'https://drive.google.com/drive/folders/1-Jwg4MmjiYG8jAiR6vCfY-MCVkPuKB90?usp=drive_link' 
+    },
+    {
+        name: 'นางสาวอรุณ สายอ่อนตา',
+        fileUrl: 'https://drive.google.com/drive/folders/1UqE-kjPZrqTRgN1B4IueSeihVXvZpxLH?usp=drive_link' 
+    },
+];
